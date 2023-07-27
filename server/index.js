052c2773983978abbd136983c5479a21b3819cd2
sabihDev <8203sabi.rahman@gmail.com>
2023-07-27 05:01:36 -0700

    js part created

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
-const express = require('express');
-const fs = require('fs').promises;
-const path = require('path');
+const express = require("express");
+const fs = require("fs").promises;
+const path = require("path");
 
 const app = express();
-const dataFile = path.join(__dirname, 'data.json');
+const dataFile = path.join(__dirname, "data.json");
 
-// Supporting POSTing data with URL encoded
+// Support POSTing form data with URL encoded
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/poll', async (req, res) => {
-    let data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
+// Enable CORS
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
+    next();
+});
+
+app.get("/poll", async (req, res) => {
+    let data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
     const totalVotes = Object.values(data).reduce((total, n) => total += n, 0);
 
     data = Object.entries(data).map(([label, votes]) => {
@@ -18,13 +24,14 @@ app.get('/poll', async (req, res) => {
             label,
             percentage: (((100 * votes) / totalVotes) || 0).toFixed(0)
         }
-    })
+    });
 
     res.json(data);
 });
 
-app.post('/poll', async (req, res) => {
+app.post("/poll", async (req, res) => {
     const data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
+
     data[req.body.add]++;
 
     await fs.writeFile(dataFile, JSON.stringify(data));
@@ -32,6 +39,4 @@ app.post('/poll', async (req, res) => {
     res.end();
 });
 
-app.listen(3000, () => {
-    console.log('Server is running');
-});
+app.listen(3000, () => console.log("Server is running..."));
